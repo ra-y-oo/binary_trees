00d8087d1f7ebdf2946ccaeffb44eab9c8d9ea91
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-06-01 20:55:37 +0300

    Create 1-binary_tree_insert-left.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,11 +3,12 @@
 
 typedef struct binary_tree_t {
     int value;
+    int *parent;
     struct binary_tree_t *left;
     struct binary_tree_t *right;
 }binary_tree_t;
 
-binary_tree_t *binary_tree_nde(binary_tree_t *parent, int value) {
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
     binary_tree_t* result = malloc(sizeof(binary_tree_t));
     if (result != NULL) {
         result-> left = NULL;
---
+++ 1-binary_tree_insert-left.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <0-binary_tree_node.c>
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value) {
+    if (parent = NULL) {
+        return NULL;
+    }
+
+    if (parent->left != NULL) {
+        binary_tree_t *value = binary_tree_node(parent->left, value);
+        if (value == NULL) {
+            return NULL;
+        }
+
+        parent->left = value;
+        return value;
+    } else {
+
+    binary_tree_t *value = binary_tree_node(parent, value);
+
+    if (value = NULL) {
+        return NULL;
+    }
+
+    parent->left = value;
+    return value;
+    }
+}
+
