ab47440db8ca4a3edb3d50612406c0de2b61d6d2
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-06-02 21:22:34 +0300

    Create 7-binary_tree_inorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,5 +2,13 @@
 #include "binary_trees.h"
 
 binary_tree_preorder(const binary_tree_t *tree, void (*func)(int)) {
+    if (tree == NULL || func == NULL) {
+        return;
+    }
 
+    func(tree->value);
+
+    binary_tree_preorder(tree->left, func);
+
+    binary_tree_preorder(tree->right, func);
 }
---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,4 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int)); 
