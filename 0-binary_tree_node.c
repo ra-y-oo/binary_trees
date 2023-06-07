0581db58a9510a24938fc0f3219b4ee5f58d175e
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-06-08 00:31:15 +0300

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -6,18 +6,17 @@
  * @value: value of new node
  * Return: pointer to new node
  */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
 	new = malloc(sizeof(binary_tree_t));
-	if (new != NULL){
-
-	new->value = value;
+	if (!(new))
+		return (NULL);
+	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
 	new->right = NULL;
-    }
-
-    return (new);
+	return (new);
 }
