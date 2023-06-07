5e6aa4eb2864ffa5c4352422a04505946148900e
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-06-08 00:15:08 +0300

    Add upyo task 18

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,50 @@
+#include "binary_trees.h"
+#include "14-binary_tree_balance.c"
+#include "15-binary_tree_is_full.c"
+
+/**
+ * check_leaves - checks if all leaves are at same level
+ * @tree: node to start
+ * @current: current level of current leaf
+ * @level: level of past leaves
+ * Return: true or false
+ */
+
+int check_leaves(const binary_tree_t *tree, int current, int *level)
+{
+
+	if (tree == NULL)
+		return (0);
+	if (tree->right == NULL && tree->left == NULL)
+	{
+		if (*level == 0)
+		{
+			*level = current;
+			return (1);
+		}
+		return (*level == current);
+	}
+	return (check_leaves(tree->left, current + 1, level) &&
+		check_leaves(tree->right, current + 1, level));
+}
+
+/**
+ * binary_tree_is_perfect - finds if tree is full or not
+ * @tree: root node
+ * Return: balance
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int level = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (binary_tree_is_full(tree) && (!binary_tree_balance(tree)))
+	{
+		return (check_leaves(tree, 0, &level));
+	}
+
+	return (0);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -1,13 +1,33 @@
-#ifndef BINARY_TREEs_H
-#define BINARY_TREEs_H
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifndef _BINARY_TREES_H
+#define _BINARY_TREES_H
 
-typedef struct binary_tree {
-    int value;
-    struct binary_tree *parent;
-    struct binary_tree *left;
-    struct binary_tree *right;
-} binary_tree_t;
 
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -18,7 +38,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
-size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *node);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
@@ -27,5 +47,4 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
-#endif /* BINARY_TREE_H */
+#endif
